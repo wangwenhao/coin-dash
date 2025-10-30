87882cf2bb9d67e152fc00073f6cba0d957fe0c0
王闻昊 <wwh27791@ly.com>
2025-10-30 16:28:24 +0800

    add tween

--- coin.gd
+++ coin.gd
@@ -3,12 +3,17 @@ extends Area2D
 var sizescreen
 
 func pickup():
-	queue_free()
+	#monitoring = false
+	var tween = get_tree().create_tween()
+	tween.tween_property($AnimatedSprite2D, "scale", $AnimatedSprite2D.scale * 3, 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	tween.set_parallel()
+	tween.tween_property($AnimatedSprite2D, "modulate", Color(1, 1, 1, 0), 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	tween.set_parallel(false)
+	tween.tween_callback(queue_free)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
